b7d029b00ad75c03a51d7c4f9265f295625f7755
FERNANDO JACINTO DA SILVA <fernando.jsilva9@senacsp.edu.br>
2025-07-24 22:20:12 -0300

    install lib axios and @react-native-async-storage/async-storage, added services/api, updated useState, handleSubmit()

--- App.tsx
+++ App.tsx
@@ -1,24 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, KeyboardAvoidingView, Platform, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 import logo from './src/assets/logo.png';
 
+import api from './src/services/api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [techs, setTechs]  = useState('');
+
+  useEffect( () =>{
+
+  },[])
+
+  async function handleSubmit(){
+    console.log(email, techs);
+
+    const response = await api.post('/session', {email})
+
+    const { _id } = response.data;
+    await AsyncStorage.setItem('user', _id);
+    await AsyncStorage.setItem('techs', techs);
+
+    //navigation.navigate('List');
+
+  }
+
   return (
-    <View style={styles.container}>
+    // enabled={Platform.OS === 'ios'}
+    <KeyboardAvoidingView behavior='padding' style={styles.container}>
       <Image source={logo}></Image>
       <View style={styles.form}>
         <Text style={styles.label}>SEU E-MAIL *</Text>
-        <TextInput style={styles.input}/>
+        <TextInput 
+          style={styles.input}
+          placeholder='Seu e-mail'
+          placeholderTextColor="#999"
+          keyboardType='email-address'
+          autoCapitalize='none'
+          autoCorrect={false}
+          value={email}
+          // onChangeText={text => setEmail(text)}
+          onChangeText={setEmail}
+        />
+          
         <Text style={styles.label}>TECNOLOGIAS *</Text>
-        <TextInput style={styles.input}/>
-        <TouchableOpacity style={styles.button}>
+        <TextInput 
+          style={styles.input}
+          placeholder='Tecnologias de interesse'
+          placeholderTextColor="#999"
+          autoCapitalize='words'
+          autoCorrect={false}
+          value={techs}
+          onChangeText={setTechs}
+        />
+        <TouchableOpacity 
+          style={styles.button}
+          // onPress={() => {}} chamar uma função vazia
+          onPress={handleSubmit}
+        >
           <Text style={styles.buttonText}>Encontrar spots</Text>
         </TouchableOpacity>
         
       </View>
       
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 

fa58c92cd312ce5e22c90e450cd12b9a1b9d6ec7
FERNANDO JACINTO DA SILVA <fernando.jsilva9@senacsp.edu.br>
2025-10-02 22:18:27 -0300

    added lib zod,react-hook-forms,datapickertimedate and pages/Book update zod

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
-import { StyleSheet, Alert, SafeAreaView, Image, Text, TextInput, TouchableOpacity } from "react-native";
+import { StyleSheet, Alert, SafeAreaView, Image, Text, Button, TextInput, TouchableOpacity, Platform } from "react-native";
 import { RootStackParamList } from "../routes";
 import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { z } from 'zod';
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 import logo from '../assets/logo.png';
 import api from '../services/api';
@@ -12,6 +17,18 @@ import api from '../services/api';
 type BookRouteProp = RouteProp<RootStackParamList, 'Book'>
 type BookScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Book'>
 
+// Schema Zod
+const bookSchema = z.object({
+    // Campo "date" obrigatório do tipo Date
+    date: z.date({
+        required_error: "Data é obrigatória"
+    })
+});
+
+// Inferência do tipo com base no schema
+// Isso permite que o Typescript entenda os dados do formulário
+// BookFormData é igual a { date: Date }
+type BookFormData = z.infer<typeof bookSchema>
 
 export function Book(){
     const [date, setDate] = useState('');
@@ -20,13 +37,33 @@ export function Book(){
     const route = useRoute<BookRouteProp>();
 
     const { id } = route.params; //id do spot(da sala pra reserva)
+
+    const [showPicker, setShowPicker] = useState(null) // controla se o seletor de data está visível
+
+    // useForm controla o formulário. Aqui informamos:
+    // - o tipo (BookFormData)
+    // - o validador (zodResolver com o schema do Zod)
+    const {
+        control, // para controlar campos individuais com <Controller />
+        handleSubmit, // função que executa a validação e chama o submit
+        formState: {errors } // objeto com os erros de validação
+    } = useForm<BookFormData>({
+        resolver: zodResolver(bookSchema),
+        defaultValues:{
+            date: new Date() //valor inicial do campo date
+        }
+    })
     
-    async function handleSubmit(){
+    // async function handleSubmit(){
+    async function onSubmit(data:BookFormData){
         const user_id = await AsyncStorage.getItem('user')
+        //converte a data para string(padrão ISO)
+        const dateFormatted = data.date.toISOString();
 
         await api.post(
             `/bookings/${id}/spots`, // rota do backend
-            { date, }, // envio dos dados pelo body para o backend
+            // { date, }, // envio dos dados pelo body para o backend
+            { dateFormatted }, // envio dos dados pelo body para o backend
             { headers: {user_id} } // headers e/ou cabeçalho, id do usuário logado
         );
 
@@ -42,7 +79,7 @@ export function Book(){
         <SafeAreaView style={styles.container}>
             <Image style={styles.logo} source={logo}/>
             <Text style={styles.label}>DATA DE INTERESSE *</Text>
-            <TextInput 
+            {/* <TextInput 
                 style={styles.input}
                 placeholder="Qual a data você quer reservar?"
                 placeholderTextColor="#999"
@@ -50,8 +87,42 @@ export function Book(){
                 autoCorrect={false}
                 value={date}
                 onChangeText={setDate}
+            /> */}
+            {/* Controller conecta o campo "date" ao react-hook-form */}
+            <Controller 
+                control={control}
+                name="date"
+                render={ ({ field: { value, onChange} }) =>(
+                    <>
+                    {/* Botão para abrir o seletor de data */}
+                    <Button title="Selecionar Data" onPress={ 
+                        () => setShowPicker(true) } 
+                    />
+                    {/* Exibe a data selecionada */}
+                    <Text style={[styles.label, {marginTop:10, marginBottom:20}]}>
+                        DATA SELECIONADA: {value.toLocaleDateString('pt-BR')}    
+                    </Text>
+
+                    {/* Se o seletor estiver visivel, exibe o component de data */}
+                    {showPicker && (
+                        <DateTimePicker 
+                         value={value}
+                         mode="date"
+                         display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+                         onChange={(event, selectedDate) =>{
+                            setShowPicker(Platform.OS === 'ios');
+                            if (selectedDate){
+                                onChange(selectedDate); // atualiza o valor do campo date
+                            }
+                         }}
+                        />
+                    )}
+                    </>
+                )}
+            
+            
             />
-            <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+            <TouchableOpacity onPress={handleSubmit(onSubmit)} style={styles.button}>
                 <Text style={styles.buttonText}>Solicitar reserva</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={handleCancel} style={[styles.button, styles.cancelButton]}>
